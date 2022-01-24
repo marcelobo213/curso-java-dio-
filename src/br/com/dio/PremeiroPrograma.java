aa00cdf79831d3b79334e5599f431cee6597d7f2
marcelo borges <marceloba4321@gmail.com>
2022-01-24 20:57:21 -0300

    first commit

---
+++ src/br/com/dio/PremeiroPrograma.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class PremeiroPrograma {
+    public static void main(String[] args) {
+        int a= 4;
+        int b= 9;
+        System.out.println("Hello World " + (a+b));
+    }
+}
