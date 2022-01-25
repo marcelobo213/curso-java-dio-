dbbf6aa3b230fd41511c776a24e31b635fe43ffb
marcelo borges <marceloba4321@gmail.com>
2022-01-24 22:02:34 -0300

    entendendo debug

---
+++ src/br/com/dio/cauculadora/calculadora.java
@@ -0,0 +1,41 @@
+package br.com.dio.cauculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplica(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("somar" + somar);
+        System.out.println("subt" + subtrair);
+        System.out.println("mult" + multiplicar);
+        System.out.println("div" + dividir);
+
+    }
+
+    public static int somar(int a, int b){
+        return a + b;
+    }
+    public static int subtrair (int a , int b){
+        return a - b;
+    }
+    public static int multiplica (int a , int b){
+        return a * b;
+    }
+    public static int dividir (int a , int b) {
+        return a / b;
+    }
+}
