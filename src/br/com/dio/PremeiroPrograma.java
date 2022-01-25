bede24e133b6919f4193565e28dfd05482c25ad3
marcelo borges <marceloba4321@gmail.com>
2022-01-24 21:29:41 -0300

    criando class livro

--- src/br/com/dio/PremeiroPrograma.java
+++ src/br/com/dio/PremeiroPrograma.java
@@ -5,5 +5,40 @@ public class PremeiroPrograma {
         int a= 4;
         int b= 9;
         System.out.println("Hello World " + (a+b));
+
+        livro livro1 = new livro("o problema dos 3 corpos", 300);
+        System.out.println(livro1);
+    }
+}
+
+class livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, int numePaginas) {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
